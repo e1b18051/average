24a17187d4e877f9e0572391130a50549a3cc2f1
shunsh65 <e1b18067@oit.ac.jp>
2020-10-06 16:04:05 +0900

    shoki

---
+++ src/main/java/oit/is/lec2_2/average/AverageApplication.java
@@ -0,0 +1,13 @@
+package oit.is.lec2_2.average;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AverageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AverageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/lec2_2/average/AverageApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.lec2_2.average;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AverageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
